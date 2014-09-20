45b6ede463becfe78988f50a92359593c606eb5a
quincho93vv <jjose93v@gmail.com>
2014-09-19 19:18:13 -0600

    Prueba

--- PGS/src/pgs/PGS.java
+++ PGS/src/pgs/PGS.java
@@ -5,7 +5,9 @@ public class PGS {
     public static void main(String[] args) {
         System.out.println("Prueba del proyecto");
         
-          
+       
+        
+        
     }
     
 }
