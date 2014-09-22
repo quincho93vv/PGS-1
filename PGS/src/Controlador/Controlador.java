fcdd1aecaa8be398743e6c71749db951c461e29e
quincho93vv <jjose93v@gmail.com>
2014-09-22 12:51:28 -0600

    Prueba

---
+++ PGS/src/Controlador/Controlador.java
@@ -0,0 +1,13 @@
+
+package Controlador;
+
+import Modelo.Modelo;
+
+public class Controlador {
+    private Modelo modelo;
+
+    public Controlador(Modelo modelo) {
+        this.modelo = modelo;
+    }
+    
+}
--- PGS/src/pgs/PGS.java
+++ PGS/src/pgs/PGS.java
@@ -1,13 +1,33 @@
 package pgs;
 
+import Controlador.Controlador;
+import Modelo.Modelo;
+import Vista.Login;
+import javax.swing.SwingUtilities;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class PGS {
 
     public static void main(String[] args) {
-        System.out.println("Prueba del proyecto");
-        
-       
-        
-        
+        try {
+          javax.swing.UIManager.setLookAndFeel("com.jtattoo.plaf.mcwin.McWinLookAndFeel");
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            System.out.println(e.getMessage());
+        }
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                mostrarInterfaz();
+            }
+        });
     }
-    
+
+    public static void mostrarInterfaz() {
+        Modelo modelo = new Modelo();
+        Controlador control = new Controlador(modelo);
+        Login v = new Login(control);
+        v.mostrar();
+    }
+
 }
